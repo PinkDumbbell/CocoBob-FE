75d4a0fbd7b82b3eadf43e4aa3f60a504f177087
은승균 <dmstmdrbs98@gmail.com>
2022-07-14 12:37:10 +0900

    CB-110 feat: add unit test case for EmailLoginForm
    이메일 로그인 유닛 테스트 케이스 추가

--- src/pages/Login/components/EmailLoginForm.test.tsx
+++ src/pages/Login/components/EmailLoginForm.test.tsx
@@ -19,8 +19,6 @@ import EmailLoginForm from './EmailLoginForm';
  *
  * case 4:
  *    이메일 입력 o, 비밀번호 입력 o => 로그인 버튼 활성화
- *
- *
  */
 describe('<EmailLoginForm />', () => {
   beforeEach(() => {
@@ -30,39 +28,27 @@ describe('<EmailLoginForm />', () => {
       </Provider>,
     );
   });
+  const setInputValues = (email: string, password: string) => {
+    fireEvent.input(screen.getByTestId('email'), {
+      target: {
+        value: email,
+      },
+    });
+    fireEvent.input(screen.getByTestId('password'), {
+      target: {
+        value: password,
+      },
+    });
+  };
 
   it('email과 password 모두 입력하지 않았을 때, 로그인 버튼 disabled', async () => {
-    act(() => {
-      fireEvent.input(screen.getByTestId('email'), {
-        target: {
-          value: '',
-        },
-      });
-      fireEvent.input(screen.getByTestId('password'), {
-        target: {
-          value: '',
-        },
-      });
-    });
-
+    act(() => setInputValues(/* email= */ '', /* password= */ ''));
     expect(await screen.findByText('로그인')).toBeDisabled();
   });
 
   // it('이메일 형식 invalid 에러');
   it('이메일 입력, 비밀번호 입력하지 않았을 때, 로그인 버튼 disabled', async () => {
-    act(() => {
-      fireEvent.input(screen.getByTestId('email'), {
-        target: {
-          value: 'test@test.com',
-        },
-      });
-      fireEvent.input(screen.getByTestId('password'), {
-        target: {
-          value: '',
-        },
-      });
-    });
-
+    act(() => setInputValues(/* email= */ 'test@test.com', /* password= */ ''));
     expect(screen.getByText('로그인')).toBeDisabled();
     act(() => {
       fireEvent.click(screen.getByText('로그인'));
@@ -75,19 +61,7 @@ describe('<EmailLoginForm />', () => {
   });
 
   it('이메일 입력하지 않았을때, 비밀번호 입력했을때, 로그인 버튼 disabled', async () => {
-    act(() => {
-      fireEvent.input(screen.getByTestId('email'), {
-        target: {
-          value: '',
-        },
-      });
-      fireEvent.input(screen.getByTestId('password'), {
-        target: {
-          value: 'password',
-        },
-      });
-    });
-
+    act(() => setInputValues(/* email= */ '', /* password= */ 'password'));
     expect(screen.getByText('로그인')).toBeDisabled();
     act(() => {
       fireEvent.click(screen.getByText('로그인'));
@@ -100,16 +74,7 @@ describe('<EmailLoginForm />', () => {
   });
 
   it('이메일 입력, 비밀번호 입력 했을때 버튼 available', async () => {
-    fireEvent.input(screen.getByTestId('email'), {
-      target: {
-        value: 'test@test.com',
-      },
-    });
-    fireEvent.input(screen.getByTestId('password'), {
-      target: {
-        value: 'password',
-      },
-    });
+    act(() => setInputValues(/* email= */ 'john@gmail.com', /* password= */ 'password'));
 
     expect(screen.getByText('로그인')).not.toBeDisabled();
   });
