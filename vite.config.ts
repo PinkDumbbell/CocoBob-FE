2a4664055df624d041af335aad0fc7d7db9ea7a3
은승균 <dmstmdrbs98@gmail.com>
2022-07-20 09:45:27 +0900

    CB-166 hotfix: fix build error
    .env의 변수들을 vite.config.ts에 매칭 시켜야 함.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
-import EnvironmentPlugin from 'vite-plugin-environment';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin('all')],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  define: {
+    'process.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   },
 });
