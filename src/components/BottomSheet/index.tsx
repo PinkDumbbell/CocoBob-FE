abb26126dca6ece9121cea51471f66042b414509
은승균 <dmstmdrbs98@gmail.com>
2022-07-20 17:43:19 +0900

    CB-129 style: add border radius theme
    border radius 추가
    - ThemeProvider 추가
    - MUI theme 커스텀 추가

--- src/components/BottomSheet/index.tsx
+++ src/components/BottomSheet/index.tsx
@@ -3,13 +3,23 @@ import { ReactNode } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import { createTheme, ThemeProvider } from '@mui/material';
 
-const BottomSheetWrapper = styled(SwipeableDrawer)`
-  .css-9emuhu-MuiPaper-root-MuiDrawer-paper {
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-  }
-`;
+const theme = createTheme({
+  components: {
+    MuiDrawer: {
+      styleOverrides: {
+        paperAnchorBottom: {
+          borderTopRightRadius: 10,
+          borderTopLeftRadius: 10,
+          paddingTop: 10,
+        },
+      },
+    },
+  },
+});
+
+const BottomSheetWrapper = styled(SwipeableDrawer)``;
 
 export default function BottomSheet({
   isOpen,
@@ -22,14 +32,16 @@ export default function BottomSheet({
   const closeBottomSheet = () => dispatch(closeBottomSheetAction());
 
   return (
-    <BottomSheetWrapper
-      anchor={'bottom'}
-      open={isOpen}
-      onClose={closeBottomSheet}
-      onOpen={() => {}}
-    >
-      {/* <BottomSheetHeader close={closeBottomSheet} /> */}
-      <div className="flex-1 overflow-auto pt-8">{children}</div>
-    </BottomSheetWrapper>
+    <ThemeProvider theme={theme}>
+      <BottomSheetWrapper
+        anchor={'bottom'}
+        open={isOpen}
+        onClose={closeBottomSheet}
+        onOpen={() => {}}
+      >
+        {/* <BottomSheetHeader close={closeBottomSheet} /> */}
+        {children}
+      </BottomSheetWrapper>
+    </ThemeProvider>
   );
 }
