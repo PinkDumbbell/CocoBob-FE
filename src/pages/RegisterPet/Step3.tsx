7959ed255854a94552943e6e66676183959cd284
은승균 <dmstmdrbs98@gmail.com>
2022-07-27 14:16:04 +0900

    CB-216 refactor: add BreedList component
    견종 리스트 컴포넌트 분리
    - 반복되는 부분 분리함

--- src/pages/RegisterPet/Step3.tsx
+++ src/pages/RegisterPet/Step3.tsx
@@ -14,6 +14,23 @@ import { ButtonWrapper, PageContainer, QuestionText, Form, PetNameHighlight } fr
 import { IPrevNextStep } from './type';
 import useSearchBreed from './hooks/useSearchBreed';
 
+interface IBreedList {
+  breeds: IBreeds[];
+  onSelectBreed: (breed: IBreeds) => void;
+}
+const BreedList = ({ breeds, onSelectBreed }: IBreedList) => (
+  <>
+    {breeds.map((breed) => (
+      <div key={breed.breedId} onClick={() => onSelectBreed(breed)}>
+        <p className="py-2">
+          <span className="pr-2 text-primary-900">{breed.breedSize}</span>
+          {breed.breedName}
+        </p>
+      </div>
+    ))}
+  </>
+);
+
 export default function Step3({ goPrevStep, goNextStep }: IPrevNextStep) {
   const { isBottomSheetOpen, openBottomSheet } = useBottomSheet('findBreed');
   const {
@@ -87,23 +104,11 @@ export default function Step3({ goPrevStep, goNextStep }: IPrevNextStep) {
             placeholder="품종을 검색해보세요"
           />
           <div className="h-[50vh] px-2 py-3 overflow-y-scroll">
-            {!searchKeyword
-              ? breeds.map((breed) => (
-                  <div key={breed.breedId} onClick={() => onSelectBreed(breed)}>
-                    <p className="py-2">
-                      <span className="pr-2 text-primary-900">{breed.breedSize}</span>
-                      {breed.breedName}
-                    </p>
-                  </div>
-                ))
-              : foundBreeds.map((breed) => (
-                  <div key={breed.breedId} onClick={() => onSelectBreed(breed)}>
-                    <p className="py-2">
-                      <span className="pr-2 text-primary-900">{breed.breedSize}</span>
-                      {breed.breedName}
-                    </p>
-                  </div>
-                ))}
+            {!searchKeyword ? (
+              <BreedList breeds={breeds} onSelectBreed={onSelectBreed} />
+            ) : (
+              <BreedList breeds={foundBreeds} onSelectBreed={onSelectBreed} />
+            )}
           </div>
           <Button label="선택 완료" />
         </div>
