e98b0bd552b63a5f16e68c142a9690f0faec9d57
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-08-03 16:42:09 +0900

    CB-238 feat : add category to  Product page

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,11 +1,43 @@
 import Layout from '@/components/layout/Layout';
 import ProductItem from '@/components/Product';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function ProductsPage() {
+  const categoryList = ['사료', '간식', '영양제'];
+  // eslint-disable-next-line no-unused-vars
+  const [category, setCategory] = useState<string>('');
+
+  useEffect(() => {});
   return (
-    <Layout footer header title="사료">
+    <Layout footer header title="사료" canSearch>
       <div>
         <div className="fixed w-full flex flex-col bg-white">
+          <div className="w-full h-12 flex items-center overflow-x-auto ">
+            {categoryList.map((categoryName) =>
+              categoryName === category ? (
+                <div
+                  key={categoryName}
+                  className="box-border h-12 w-32 text-center leading-[3rem] border-b border-red-500 text-red-500"
+                  onClick={() => {
+                    setCategory(categoryName);
+                  }}
+                >
+                  {categoryName}
+                </div>
+              ) : (
+                <div
+                  key={categoryName}
+                  className="w-32 text-center leading-[3rem]"
+                  onClick={() => {
+                    setCategory(categoryName);
+                  }}
+                >
+                  {categoryName}
+                </div>
+              ),
+            )}
+          </div>
           <div className="p-1 w-full h-8 border border-gray-200 flex items-center justify-end">
             <div className="flex items-center gap-2">
               <label htmlFor="affco-filter" className="text-gray-700 text-[0.8rem]">
@@ -15,7 +47,7 @@ export default function ProductsPage() {
             </div>
           </div>
         </div>
-        <div className="pt-9 flex flex-col gap-1">
+        <div className="pt-20 flex flex-col gap-1">
           {Array(12)
             .fill(0)
             .map((_, idx) => (
