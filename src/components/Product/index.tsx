d39d739e1f618b484d05322c30fea7d1175e1f20
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-08-08 15:37:13 +0900

    CB-238 feat : modify product item - 기존 더미데이터에서 api 요청을 보낸 데이터로 수정

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,3 +1,4 @@
+import { IProduct } from '@/@type/product';
 import ChipButton from '../ChipButton';
 
 /**
@@ -8,20 +9,20 @@ import ChipButton from '../ChipButton';
  * - AFFCO 만족/불만족
  * - 유아/성견/노견
  */
-export default function ProductItem() {
+interface IProductItem {
+  product: IProduct;
+}
+export default function ProductItem(props: IProductItem) {
+  const { product } = props;
   return (
     <div className="flex w-full h-36 border-b border-gray-200">
       <div className="w-36 h-36 flex justify-center items-center p-1">
-        <img
-          className="w-full"
-          src="https://img.dogpre.com/mobile/dogpre/product/60/59983_originalView_01078322.jpg"
-          alt=""
-        />
+        <img className="w-full" src={product.thumbnail} alt="" />
       </div>
       <div className="flex flex-col p-1 justify-center gap-2">
         <div className="flex flex-col gap-0.5">
-          <h4 className="text-black font-bold">로얄캐닌 미니 인도어 어덜트 3kg 소화기 건강</h4>
-          <p>35,800원</p>
+          <h4 className="text-black font-bold w-60">{product.name}</h4>
+          <p>{product.price.toLocaleString('ko-KR')}원</p>
         </div>
         <div className="flex gap-1 items-center">
           <ChipButton content="성견" filled={false} border />
