05c6449691709456bf9f8a527eba0cfc927a6dd6
은승균 <dmstmdrbs98@gmail.com>
2022-08-11 16:21:06 +0900

    CB-308 style: modify styles of calendar tile
    캘린더 타일 스타일 수정
    - 원 크기 조절
    - 버튼 색상 수정

--- src/pages/Daily/Daily.tsx
+++ src/pages/Daily/Daily.tsx
@@ -16,7 +16,7 @@ const CalendarWrapper = ({
   <div>
     <div
       className={
-        'overflow-hidden transition-all flex items-start justify-center rounded-[10px] w-full '
+        'overflow-hidden transition-all flex items-start justify-center rounded-[10px] w-full'
       }
     >
       <Calendar
@@ -30,11 +30,11 @@ const CalendarWrapper = ({
           // 날짜 타일에 컨텐츠 추가하기 (html 태그)
           // 추가할 html 태그를 변수 초기화
           const html = [];
-          if (dayjs(date).format('YYYY/MM/DD') === dayjs(currentDate).format('YYYY/MM/DD')) {
+          if (dayjs(date).format('YYYY/MM/DD') <= dayjs(new Date()).format('YYYY/MM/DD')) {
             html.push(
               <div
                 key={dayjs(date).format('YYYY/MM/DD')}
-                className="h-1.5 w-1.5 rounded-full bg-emerald-400"
+                className="z-0 opacity-50 rounded-full absolute w-10 h-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2  bg-primary-light"
               ></div>,
             );
           }
