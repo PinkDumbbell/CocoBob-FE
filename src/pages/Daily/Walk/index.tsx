f5f82a341bcbcbbf0a17dc3fa305d2735161e932
은승균 <dmstmdrbs98@gmail.com>
2022-08-11 14:54:37 +0900

    CB-310 feat: add Kakao Map API with some settings
    카카오 지도 API 적용 및 설정 추가
    - env 변수를 index.html에서 사용하기 위한 플러그인 설정 추가
    - 카카오 지도를 사용하기 위한 커스텀 훅 추가
      - useCurrentPosition
      - useWatchPosition

--- src/pages/Daily/Walk/index.tsx
+++ src/pages/Daily/Walk/index.tsx
@@ -1,9 +1,53 @@
+/* eslint-disable no-new */
 import Layout from '@/components/layout/Layout';
+// import useCurrentLocation from '@/utils/hooks/useCurrentPosition';
+import useWatchPosition from '@/utils/hooks/useWatchPosition';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 export default function DailyWalk() {
+  // const { location:, error } = useCurrentLocation({
+  //   enableHighAccuracy: true,
+  //   maximumAge: 10000,
+  //   timeout: 5000,
+  // });
+  const { location, error } = useWatchPosition({
+    enableHighAccuracy: false,
+    maximumAge: 10000,
+    timeout: 5000,
+  });
+  const { latitude, longitude } = location;
+
   return (
     <Layout header title="산책하기" canGoBack>
-      <div className="bg-white">산책 페이지</div>
+      <div className="bg-white h-full flex flex-col w-full">
+        <div className="h-4/5 w-full">
+          {error && (
+            <div className="w-full h-full bg-gray-300 flex items-center justify-center">
+              GPS를 사용할 수 없습니다!
+            </div>
+          )}
+          {!error && (
+            <Map
+              center={{
+                lat: latitude,
+                lng: longitude,
+              }}
+              maxLevel={5}
+              style={{ width: '100%', height: '100%' }}
+            >
+              <MapMarker position={{ lat: latitude, lng: longitude }}></MapMarker>
+            </Map>
+          )}
+        </div>
+        <div className="h-1/5 w-full bg-white flex items-center justify-around">
+          <button
+            type="button"
+            className="rounded-full w-24 h-24 bg-primary-bright flex items-center justify-center"
+          >
+            <span className="text-white font-medium text-[18px]">시작</span>
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 }
---
+++ src/utils/hooks/useCurrentPosition.tsx
@@ -0,0 +1,40 @@
+import { IGeolocationOptions, ILocation } from '@/@type/location';
+import { useState, useEffect, useCallback } from 'react';
+
+const defaultPosition: ILocation = {
+  latitude: 37.5036833,
+  longitude: 127.0448556,
+};
+
+const useCurrentLocation = (options: IGeolocationOptions = {}) => {
+  const [location, setLocation] = useState<ILocation>(defaultPosition);
+  const [error, setError] = useState<string>();
+
+  // Geolocation의 `getCurrentPosition` 메소드에 대한 성공 callback 핸들러
+  const handleSuccess = useCallback((pos: { coords: ILocation }) => {
+    const { latitude, longitude } = pos.coords;
+    setLocation({
+      latitude,
+      longitude,
+    });
+  }, []);
+
+  // getCurrentPosition 실패 callback 핸들러
+  const handleError = useCallback((err: { message: string }) => {
+    setError(err.message);
+  }, []);
+
+  useEffect(() => {
+    const { geolocation } = navigator;
+    // 사용된 브라우저에서 Geolocation이 정의되지 않은 경우 오류로 처리
+    if (!geolocation) {
+      setError('Geolocation is not supported.');
+      return;
+    }
+    geolocation.getCurrentPosition(handleSuccess, handleError, options);
+  }, [options]);
+
+  return { location, error };
+};
+
+export default useCurrentLocation;
