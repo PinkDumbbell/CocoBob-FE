79036cf17fe0a1f3f6e8a55abb28395132ef3af6
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-08-12 14:48:16 +0900

    CB-238 feat : Modify style and navigate to ProductDetailPage

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -2,6 +2,7 @@ import Layout from '@/components/layout/Layout';
 import ProductItem from '@/components/Product';
 import { useGetProductQuery } from '@/store/api/productApi';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProductsPage() {
   const categoryList = ['사료', '간식', '영양제'];
@@ -9,53 +10,56 @@ export default function ProductsPage() {
   const [category, setCategory] = useState<string>('');
   const { data } = useGetProductQuery();
   // todo: 검색 가능한 키워드 선정하기
+  const navigate = useNavigate();
   const productList = data?.productList;
   useEffect(() => {});
   return (
     <Layout footer header title="사료" canSearch>
-      <div>
-        <div className="fixed w-full flex flex-col bg-white">
-          <div className="w-full h-12 flex items-center overflow-x-auto ">
-            {categoryList.map((categoryName) =>
-              categoryName === category ? (
-                <div
-                  key={categoryName}
-                  className="box-border h-12 w-32 text-center leading-[3rem] border-b border-red-500 text-red-500"
-                  onClick={() => {
-                    setCategory(categoryName);
-                  }}
-                >
-                  {categoryName}
-                </div>
-              ) : (
-                <div
-                  key={categoryName}
-                  className="w-32 text-center leading-[3rem]"
-                  onClick={() => {
-                    setCategory(categoryName);
-                  }}
-                >
-                  {categoryName}
-                </div>
-              ),
-            )}
-          </div>
-          <div className="p-1 w-full h-8 border border-gray-200 flex items-center justify-end">
-            <div className="flex items-center gap-2">
-              <label htmlFor="affco-filter" className="text-gray-700 text-[0.8rem]">
-                AFFCO 만족 상품
-              </label>
-              <input type="checkbox" name="" id="affco-filter" />
-            </div>
+      <div className="px-4 fixed flex flex-col w-[inherit] max-w-[425px] bg-white">
+        <div className="h-12 w-full flex justify-between items-center overflow-x-scroll">
+          {categoryList.map((categoryName) =>
+            categoryName === category ? (
+              <div
+                key={categoryName}
+                className="box-border h-12 w-[33%] text-center leading-[3rem] border-b border-red-500 text-red-500"
+                onClick={() => {
+                  setCategory(categoryName);
+                }}
+              >
+                {categoryName}
+              </div>
+            ) : (
+              <div
+                key={categoryName}
+                className="text-center leading-[3rem] w-[33%]"
+                onClick={() => {
+                  setCategory(categoryName);
+                }}
+              >
+                {categoryName}
+              </div>
+            ),
+          )}
+        </div>
+        <div className="w-full h-8 border-t border-b border-gray-200 flex items-center justify-end">
+          <div className="flex items-center">
+            <label htmlFor="affco-filter" className="text-gray-700 text-[0.8rem]">
+              AFFCO 만족 상품
+            </label>
+            <input type="checkbox" name="" id="affco-filter" />
           </div>
         </div>
-        <div className="pt-20 flex flex-col gap-1">
-          {productList?.map((product) => (
-            <div key={product.productId} className="px-2">
-              <ProductItem product={product} />
-            </div>
-          ))}
-        </div>
+      </div>
+      <div className="pt-20 flex flex-col gap-1">
+        {productList?.map((product) => (
+          <div
+            key={product.productId}
+            className="px-2"
+            onClick={() => navigate(`/product/${product.productId}`)}
+          >
+            <ProductItem product={product} />
+          </div>
+        ))}
       </div>
     </Layout>
   );
