9f9b0c329a6ae3ac9dc530a05c983d04e8f6a07f
은승균 <dmstmdrbs98@gmail.com>
2022-08-12 13:32:52 +0900

    CB-316 feat: close Toast popup when page moved
    페이지 이동 시 Toast팝업 닫기

--- src/utils/hooks/useToastConfirm.tsx
+++ src/utils/hooks/useToastConfirm.tsx
@@ -2,8 +2,10 @@ import { useAppSelector } from '@/store/config';
 import { addConfirmAction, closeConfirmAction, getToast } from '@/store/slices/toastSlice';
 import { useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 export default function useToastConfirm(callback = () => {}) {
+  const location = useLocation();
   const dispatch = useDispatch();
   const { executeCallback } = useAppSelector(getToast);
 
@@ -16,6 +18,12 @@ export default function useToastConfirm(callback = () => {}) {
     callback();
     dispatch(closeConfirmAction());
   }, [executeCallback]);
+
+  useEffect(() => {
+    return () => {
+      dispatch(closeConfirmAction());
+    };
+  }, [location.pathname]);
 
   return openConfirm;
 }
