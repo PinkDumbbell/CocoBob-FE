2cc734f728a9d68ced0a2b1e21e53c0ce6f489a9
은승균 <dmstmdrbs98@gmail.com>
2022-08-13 18:36:45 +0900

    hotfix: modify api url

--- src/pages/Login/components/SocialLoginForm.tsx
+++ src/pages/Login/components/SocialLoginForm.tsx
@@ -4,13 +4,13 @@ export default function SocialLoginForm() {
   return (
     <FormWrapper>
       <SocialLoginButton>
-        <a href={`${import.meta.env.VITE_API_BASE_URL}/users/google`}>구글 계정으로 로그인</a>
+        <a href={`${import.meta.env.VITE_API_BASE_URL}/v1/users/google`}>구글 계정으로 로그인</a>
       </SocialLoginButton>
       <SocialLoginButton className="kakao">
-        <a href={`${import.meta.env.VITE_API_BASE_URL}/users/kakao`}>카카오 계정으로 로그인</a>
+        <a href={`${import.meta.env.VITE_API_BASE_URL}/v1/users/kakao`}>카카오 계정으로 로그인</a>
       </SocialLoginButton>
       <SocialLoginButton className="apple">
-        <a href={`${import.meta.env.VITE_API_BASE_URL}/users/apple`}>애플 계정으로 로그인</a>
+        <a href={`${import.meta.env.VITE_API_BASE_URL}/v1/users/apple`}>애플 계정으로 로그인</a>
       </SocialLoginButton>
     </FormWrapper>
   );
