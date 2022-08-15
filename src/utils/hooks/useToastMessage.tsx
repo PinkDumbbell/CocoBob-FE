f5279006d48eb73678aa7418e3185f834d6169b1
은승균 <eunseung-gyun@eunseung-gyun-ui-Macmini.local>
2022-08-16 01:21:19 +0900

    hotfix: delete hooks related with router  in toast popup hook
    구조 수정 및 빌드 에러 해결을 위해 useLocation 훅 삭제
    - 새로운 코드가 기존의 테스트 코드 통과를 하지 못함

--- src/utils/hooks/useToastMessage.tsx
+++ src/utils/hooks/useToastMessage.tsx
@@ -1,7 +1,6 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-import { addToastAction, deleteToastAction } from '@/store/slices/toastSlice';
+import { addToastAction } from '@/store/slices/toastSlice';
 import { v4 as uuidv4 } from 'uuid';
 
 interface ToastProps {
@@ -9,19 +8,12 @@ interface ToastProps {
 }
 
 export default function useToastMessage(option?: ToastProps) {
-  const location = useLocation();
   const dispatch = useDispatch();
   const openToast = useCallback((content: string, type?: 'success' | 'error') => {
     dispatch(
-      addToastAction({ id: uuidv4(), content, time: option?.time ?? 3000, type: type ?? 'error' }),
+      addToastAction({ id: uuidv4(), content, time: option?.time ?? 2000, type: type ?? 'error' }),
     );
   }, []);
-
-  useEffect(() => {
-    return () => {
-      dispatch(deleteToastAction());
-    };
-  }, [location.pathname]);
 
   return openToast;
 }
