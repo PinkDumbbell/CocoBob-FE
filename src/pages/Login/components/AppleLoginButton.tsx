a394af4f552bccbc245ae90d3d634425ec1c026a
은승균 <eunseung-gyun@eunseung-gyun-ui-Macmini.local>
2022-08-16 15:46:15 +0900

    hotfix: apple login redirect test with usePopup

--- src/pages/Login/components/AppleLoginButton.tsx
+++ src/pages/Login/components/AppleLoginButton.tsx
@@ -1,5 +1,13 @@
 import AppleSignin from 'react-apple-signin-auth';
 import styled from 'styled-components';
+import axios from '@/utils/api';
+import { IAuthenticatedUser } from '@/@type/user';
+import { IGenericResponse } from '@/store/api/types';
+import { useAppDispatch } from '@/store/config';
+import { setCredentials } from '@/store/slices/authSlice';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import useToastMessage from '@/utils/hooks/useToastMessage';
 
 const ButtonWrap = styled.div`
   width: 100%;
@@ -15,23 +23,53 @@ const ButtonWrap = styled.div`
     width: 100%;
   }
 `;
-const AppleLoginButton = () => (
-  <ButtonWrap>
-    <AppleSignin
-      authOptions={{
-        clientId: 'us.petalog.services',
-        scope: 'email name',
-        redirectURI: 'https://petalog.us/auth/apple/callback',
-        usePopup: true,
-      }}
-      uiType="dark"
-      className="apple-auth-btn"
-      noDefaultStyle={false}
-      buttonExtraChildren="Apple로 로그인"
-      onSuccess={(response: any) => {
-        console.log('after popup', response);
-      }}
-    />
-  </ButtonWrap>
-);
+const AppleLoginButton = () => {
+  const openToast = useToastMessage();
+  const [login, setLogin] = useState(false);
+  const dispatch = useAppDispatch();
+
+  return login ? (
+    <Navigate to="/" replace />
+  ) : (
+    <ButtonWrap>
+      <AppleSignin
+        authOptions={{
+          clientId: 'us.petalog.services',
+          scope: 'email name',
+          redirectURI: 'https://petalog.us/auth/apple/callback',
+          usePopup: true,
+          state: 'state',
+        }}
+        uiType="dark"
+        className="apple-auth-btn"
+        noDefaultStyle={false}
+        buttonExtraChildren="Apple로 로그인"
+        onError={() => openToast('로그인 중 에러가 발생하였습니다. 잠시 후 다시 시도해주세요')}
+        onSuccess={async (response: any) => {
+          console.log('after popup', response);
+          const { authorization, user } = response as {
+            authorization: { code: string; id_token: string };
+            user?: { email: string; name: { firstName: string; lastName: string } };
+          };
+          const payload = new FormData();
+
+          payload.append('code', authorization.code);
+          if (user) {
+            payload.append('user', JSON.stringify(user));
+          }
+          const {
+            data: { data },
+          } = await axios.post<IGenericResponse<IAuthenticatedUser>>(
+            `/users/login/oauth/apple`,
+            payload,
+          );
+          if (data.userId) {
+            dispatch(setCredentials(data));
+            setLogin(true);
+          }
+        }}
+      />
+    </ButtonWrap>
+  );
+};
 export default AppleLoginButton;
