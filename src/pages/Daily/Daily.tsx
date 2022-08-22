d5164196a2bacb17a1487fff5b83030c8ae5793c
은승균 <eunseung-gyun@eunseung-gyun-ui-Macmini.local>
2022-08-22 11:23:29 +0900

    CB-361 style: add new design in daily page
    데일리 페이지 새로운 디자인 적용

--- src/pages/Daily/Daily.tsx
+++ src/pages/Daily/Daily.tsx
@@ -1,62 +1,66 @@
 import { useEffect } from 'react';
-import Calendar from 'react-calendar';
-import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+// import Calendar from 'react-calendar';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import dayjs from 'dayjs';
 import Layout from '@/components/layout/Layout';
 import { useAppDispatch, useAppSelector } from '@/store/config';
 import { getDailyDate, getDailyDateString, setDate, setToday } from '@/store/slices/dailySlice';
 import { getDateString } from '@/utils/libs/date';
+import { ReactComponent as PencilIcon } from '@/assets/icon/pencil_icon.svg';
+import { ReactComponent as CalendarPlusIcon } from '@/assets/icon/calendar_plus.svg';
+import { ReactComponent as CalendarPrimary } from '@/assets/icon/calendar_plus_primary.svg';
+import { ReactComponent as DogIcon } from '@/assets/icon/dog_icon.svg';
 
 import 'react-calendar/dist/Calendar.css';
 import './calendar.css';
 
-const CalendarWrapper = ({ currentDate }: { currentDate: Date }) => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const changeDate = (selectedDate: Date) => {
-    const dateString = getDateString(selectedDate);
-    dispatch(setDate({ date: dateString }));
-    navigate(`/daily?date=${dateString}`);
-  };
-  return (
-    <div>
-      <div
-        className={
-          'overflow-hidden transition-all flex items-start justify-center rounded-[10px] w-full'
-        }
-      >
-        <Calendar
-          value={currentDate}
-          defaultValue={currentDate}
-          onChange={changeDate}
-          formatDay={(locale, date) => dayjs(date).format('DD')}
-          minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
-          maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
-          tileContent={({ date }) => {
-            // 날짜 타일에 컨텐츠 추가하기 (html 태그)
-            // 추가할 html 태그를 변수 초기화
-            const html = [];
-            if (dayjs(date).format('YYYY/MM/DD') <= dayjs(new Date()).format('YYYY/MM/DD')) {
-              html.push(
-                <div
-                  key={dayjs(date).format('YYYY/MM/DD')}
-                  className="z-0 opacity-50 rounded-full absolute w-10 h-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2  bg-primary-light"
-                ></div>,
-              );
-            }
-            // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
-            return (
-              <>
-                <div className="h-full flex flex-col justify-around items-center">{html}</div>
-              </>
-            );
-          }}
-        />
-      </div>
-      <div className="mt-2 text-sm">{dayjs(currentDate).format('YYYY년 MM월 DD일')}</div>
-    </div>
-  );
-};
+// const CalendarWrapper = ({ currentDate }: { currentDate: Date }) => {
+//   const dispatch = useAppDispatch();
+//   const navigate = useNavigate();
+//   const changeDate = (selectedDate: Date) => {
+//     const dateString = getDateString(selectedDate);
+//     dispatch(setDate({ date: dateString }));
+//     navigate(`/daily?date=${dateString}`);
+//   };
+//   return (
+//     <div>
+//       <div
+//         className={
+//           'overflow-hidden transition-all flex items-start justify-center rounded-[10px] w-full'
+//         }
+//       >
+//         <Calendar
+//           value={currentDate}
+//           defaultValue={currentDate}
+//           onChange={changeDate}
+//           formatDay={(locale, date) => dayjs(date).format('DD')}
+//           minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+//           maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+//           tileContent={({ date }) => {
+//             // 날짜 타일에 컨텐츠 추가하기 (html 태그)
+//             // 추가할 html 태그를 변수 초기화
+//             const html = [];
+//             if (dayjs(date).format('YYYY/MM/DD') <= dayjs(new Date()).format('YYYY/MM/DD')) {
+//               html.push(
+//                 <div
+//                   key={dayjs(date).format('YYYY/MM/DD')}
+//                   className="z-0 opacity-50 rounded-full absolute w-10 h-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2  bg-primary-light"
+//                 ></div>,
+//               );
+//             }
+//             // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
+//             return (
+//               <>
+//                 <div className="h-full flex flex-col justify-around items-center">{html}</div>
+//               </>
+//             );
+//           }}
+//         />
+//       </div>
+//       <div className="mt-2 text-sm">{dayjs(currentDate).format('YYYY년 MM월 DD일')}</div>
+//     </div>
+//   );
+// };
 
 export default function DailyMain() {
   const dispatch = useAppDispatch();
@@ -66,20 +70,6 @@ export default function DailyMain() {
   const currentDate = useAppSelector(getDailyDate);
   const currentDateString = useAppSelector(getDailyDateString);
   const queryStringDate = searchParams.get('date');
-  const menus: { title: string; path: string }[] = [
-    {
-      title: '산책하기',
-      path: `/daily/walk?date=${currentDateString}`,
-    },
-    {
-      title: '급여량 기록',
-      path: '/daily/feeds',
-    },
-    {
-      title: '몸무게 기록',
-      path: '/daily/bodyWeight',
-    },
-  ];
 
   /**
    * url 접속 시, getDateString(currentDate) !== queryStringDate, queryStringDate로 날짜 설정
@@ -101,18 +91,63 @@ export default function DailyMain() {
 
   return (
     <Layout header footer title="데일리 기록">
-      <div className="py-4 bg-white">
-        <div className="p-4 border-b border-b-slate-200">
-          <CalendarWrapper currentDate={currentDate} />
-        </div>
-        <div>
-          {menus.map((menu) => (
-            <div key={menu.title} className="w-full py-3 px-4 border-b-slate-200 border-b">
-              <Link to={menu.path} className="block">
-                {menu.title}
-              </Link>
+      <div className="p-4 bg-white flex flex-col items-center justify-center gap-8 w-full h-full">
+        <div className="flex flex-col border rounded-[10px] h-3/4 max-h-[600px] w-full bg-white shadow-lg rounded-[10px]">
+          <div className="w-full h-16 bg-primary-dark shadow-md rounded-t-[10px] flex items-center justify-center">
+            <h4 className="text-white font-[400] text-lg">
+              {dayjs(currentDate).format('YYYY. MM. DD')}
+            </h4>
+          </div>
+          <div className="bg-white flex-1 flex flex-col p-2 px-10">
+            <div className="flex py-8 border-b flex-col items-start w-full gap-2">
+              <h5 className="text-left w-full">안녕하세요 오늘의 기록입니다.</h5>
+              <p className="text-gray-500 text-sm">
+                저는 오늘 광화문 근천에서 산책을 했어요. 날씨가 너무 더워서 고생했네요
+              </p>
             </div>
-          ))}
+            <div className="py-2 flex flex-col gap-2">
+              <h5 className="text-primary-dark">몸무게</h5>
+              <div className="w-full h-12 rounded-[10px] bg-gray-200"></div>
+            </div>
+            <div className="py-2 flex flex-col gap-2">
+              <h5 className="text-primary-dark">급여량</h5>
+              <div className="flex gap-1 items-center justify-between">
+                <div className="w-3/4 h-3 bg-primary-main rounded-[10px]"></div>
+                <span className="text-sm">381 Kcal</span>
+              </div>
+            </div>
+            <div className="py-2 flex flex-col gap-2">
+              <h5>산책</h5>
+              <div className="flex flex-col gap-2">
+                <div className="w-full flex gap-2">
+                  <div>
+                    <CalendarPrimary />
+                  </div>
+                  <div className="flex gap-1">
+                    <button>
+                      <span>산책 01</span>
+                    </button>
+                  </div>
+                </div>
+                <div className="flex items-center justify-center w-full gap-2">
+                  <span>1.6km</span>
+                  <span>307cal</span>
+                  <span>37min</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-4 items-center w-full justify-center">
+          <button className="rounded-[10px] shadow-md w-12 h-12 flex items-center justify-center bg-primary-dark">
+            <PencilIcon />
+          </button>
+          <button className="rounded-[10px] shadow-md w-12 h-12 flex items-center justify-center bg-primary-dark">
+            <CalendarPlusIcon />
+          </button>
+          <button className="rounded-[10px] shadow-md w-12 h-12 flex items-center justify-center bg-primary-dark">
+            <DogIcon />
+          </button>
         </div>
       </div>
     </Layout>
