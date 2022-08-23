7ed254bb969c64049367ea0e501820787ab28fd1
은승균 <dmstmdrbs98@gmail.com>
2022-08-23 19:59:20 +0900

    CB-363 feat: add modals for daily record
    데일리 기록을 위한 모달 추가
    - 일기, 산책, 급여량, 몸무게
    - 반복되는 컴포넌트 분리 조금 더 고민 필요

--- src/pages/Daily/components/DailyAddBodyWeight.tsx
+++ src/pages/Daily/components/DailyAddBodyWeight.tsx
@@ -1,13 +1,39 @@
+import { useForm } from 'react-hook-form';
 import FormInput from '@/components/Form/FormInput';
-import DailyModal, { DailyModalProps } from './DailyModal';
+import DailyModal, { useDailyMutation, DailyModalProps } from './DailyModal';
 
-export default function DailyBodyWeight({ onSubmit, closeModal }: DailyModalProps) {
+export default function DailyBodyWeight({ closeModal, todayDaily, date, petId }: DailyModalProps) {
+  const { register, getValues } = useForm();
+  const createOrUpdateDaily = useDailyMutation();
+
+  const saveDaily = () => {
+    const bodyWeight = getValues('bodyWeight');
+    const newDaily = {
+      ...todayDaily?.data,
+    };
+    newDaily.bodyWeight = bodyWeight;
+    createOrUpdateDaily(newDaily, petId, date, todayDaily?.dailyId);
+    closeModal();
+  };
   return (
-    <DailyModal closeModal={closeModal} onSubmit={onSubmit}>
+    <DailyModal closeModal={closeModal} onSubmit={saveDaily}>
       <div className="p-2 flex flex-col w-full items-center gap-2">
         <h3>몸무게기록</h3>
-        <div className="flex flex-col">
-          <FormInput name="walktime" label="몸무게를 입력하세요" />
+        <div className="flex gap-1 items-center">
+          <FormInput
+            label="몸무게"
+            unit="g"
+            name="bodyWeight"
+            rules={register('bodyWeight', {
+              required: '몸무게를 입력하세요',
+              onChange: (event) => {
+                const { target: value } = event;
+                const onlyNumberRegex = /\d/;
+                if (!onlyNumberRegex.test(value)) return '';
+                return value;
+              },
+            })}
+          />
         </div>
       </div>
     </DailyModal>
--- src/pages/Daily/components/DailyModal.tsx
+++ src/pages/Daily/components/DailyModal.tsx
@@ -1,15 +1,61 @@
+import {
+  DailyDataType,
+  DailyItemType,
+  useCreateDailyMutation,
+  useUpdateDailyMutation,
+} from '@/store/api/dailyApi';
+import dayjs from 'dayjs';
 import { ReactNode } from 'react';
 
 export type DailyModalProps = {
   closeModal: () => void;
-  onSubmit: () => void;
+  todayDaily?: DailyItemType;
+  date: Date;
+  petId: number | null;
 };
-type DailyModalWrapperProps = DailyModalProps & { children: ReactNode };
+type DailyModalWrapperProps = {
+  closeModal: () => void;
+  onSubmit: () => void;
+  children: ReactNode;
+};
+
+export function useDailyMutation() {
+  const [create] = useCreateDailyMutation();
+  const [update] = useUpdateDailyMutation();
+
+  const createDaily = (newDaily: DailyDataType, date: Date, petId: number) => {
+    console.log('create', newDaily, date, petId);
+    create({
+      petId,
+      body: { data: JSON.stringify(newDaily), date: dayjs(date).format('YYYY-MM-DD') },
+    });
+  };
+  const updateDaily = (newDaily: DailyDataType, date: Date, dailyId: number) => {
+    console.log('update');
+    update({
+      dailyId,
+      body: {
+        data: JSON.stringify(newDaily),
+        date: dayjs(date).format('YYYY-MM-DD'),
+      },
+    });
+  };
+  const createOrUpdateDaily = (
+    newDaily: DailyDataType,
+    petId: number | null,
+    date: Date,
+    dailyId?: number,
+  ) => {
+    if (!dailyId && date && petId) createDaily(newDaily, date, petId);
+    else if (dailyId) updateDaily(newDaily, date, dailyId);
+  };
+  return createOrUpdateDaily;
+}
 export default function DailyModal({ closeModal, onSubmit, children }: DailyModalWrapperProps) {
   return (
-    <div className="z-[9000] flex flex-col items-center justify-center w-full h-full fixed top-0 bg-[#00000029] p-2">
+    <div className="z-[9000] max-w-[425px] flex flex-col items-center justify-center w-full h-full fixed top-0 bg-[#00000029] p-4">
       <div className="w-full bg-white flex flex-col items-center justify-between rounded-[10px]">
-        <div className="flex-1">{children}</div>
+        <div className="flex-1 w-full">{children}</div>
         <div className="pt-2 w-full flex items-center justify-evenly rounded-b-[10px]">
           <button
             className="py-1 border-t border-gray-200 flex-1 rounded-bl-[10px] bg-gray-700 text-white"
