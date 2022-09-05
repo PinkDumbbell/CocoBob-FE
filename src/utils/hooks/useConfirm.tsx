4d0f6f87eca4ab2f64400ebb46c502d071eaf05a
은승균 <dmstmdrbs98@gmail.com>
2022-09-05 18:44:55 +0900

    CB-373 refactor: add esc key event listener
    esc 키 입력 시 모달 닫음

--- src/utils/hooks/useConfirm.tsx
+++ src/utils/hooks/useConfirm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import { ReactNode, useContext } from 'react';
 import { useAppDispatch } from '@/store/config';
 import { setPopupOpened } from '@/store/slices/confirmSlice';
@@ -12,20 +13,27 @@ export default function useConfirm() {
   const dispatch = useAppDispatch();
   const confirmContext = useContext(ConfirmContext);
 
-  const openPopup = async ({ title, contents }: ConfirmOpenProps) => {
+  function onKeyDownESC(event: KeyboardEvent) {
+    if (event.key !== 'Escape') return;
+    hidePopup();
+  }
+
+  function openPopup({ title, contents }: ConfirmOpenProps) {
     confirmContext.title = title;
     confirmContext.contents = contents;
     dispatch(setPopupOpened(true));
+    window.addEventListener('keydown', onKeyDownESC);
     return new Promise((resolve) => {
       confirmContext.promiseInfo = {
         resolve,
       };
     });
-  };
+  }
 
-  const hidePopup = () => {
+  function hidePopup() {
     dispatch(setPopupOpened(false));
-  };
+    window.removeEventListener('keydown', onKeyDownESC);
+  }
 
   return [openPopup, hidePopup];
 }
--- src/utils/hooks/useSelectModal.tsx
+++ src/utils/hooks/useSelectModal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import { SelectModalContext } from '@/components/SelectModal/SelectModalPortal';
 import { useAppDispatch } from '@/store/config';
 import { setSelectModalOpened } from '@/store/slices/selectModalSlice';
@@ -7,21 +8,28 @@ export default function useSelectModal() {
   const dispatch = useAppDispatch();
   const context = useContext(SelectModalContext);
 
-  const closeModal = () => {
+  function onKeyDownESC(event: KeyboardEvent) {
+    if (event.key !== 'Escape') return;
+    closeModal();
+  }
+
+  function closeModal() {
     context.menus = [];
     context.promise = {};
     dispatch(setSelectModalOpened(false));
-  };
+    window.removeEventListener('keydown', onKeyDownESC);
+  }
 
-  const openSelectModal = (menus: string[]) => {
+  function openSelectModal(menus: string[]) {
     context.menus = menus;
+    window.addEventListener('keydown', onKeyDownESC);
     return new Promise((resolve) => {
       context.promise = {
         resolve,
       };
       dispatch(setSelectModalOpened(true));
     });
-  };
+  }
 
   return [openSelectModal, closeModal];
 }
