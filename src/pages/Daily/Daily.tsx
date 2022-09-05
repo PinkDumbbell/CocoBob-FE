ab4e98c0fa7d3d9fc13e6a97204916adb72207a2
은승균 <dmstmdrbs98@gmail.com>
2022-09-05 15:44:53 +0900

    CB-361 refactor: modify daily calendar style and add select modal
    데일리 기록 캘린더 스타일 수정
    - 일요일이 주 첫번째 날이 되도록 수정
    - 월이 바뀔때 refetch하도록 수정
    
    메뉴 선택 모달 적용

--- src/pages/Daily/Daily.tsx
+++ src/pages/Daily/Daily.tsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import dayjs from 'dayjs';
+
 import Layout from '@/components/layout/Layout';
 import { useAppSelector } from '@/store/config';
-import { getDateString } from '@/utils/libs/date';
-import { ReactComponent as FoodIcon } from '@/assets/icon/dog_food.svg';
-import { ReactComponent as PencilIcon } from '@/assets/icon/pencil_icon.svg';
-import { ReactComponent as WeightIcon } from '@/assets/icon/weight_icon.svg';
-import { ReactComponent as CalendarPrimary } from '@/assets/icon/calendar_plus_primary.svg';
-import { ReactComponent as DogIcon } from '@/assets/icon/dog_icon.svg';
 import { getCurrentPet } from '@/store/slices/userSlice';
 import {
   DailyItemType,
   useLazyGetDailyListQuery,
   useLazyGetDailyQuery,
 } from '@/store/api/dailyApi';
+import { getDateString } from '@/utils/libs/date';
+import useSelectModal from '@/utils/hooks/useSelectModal';
+import { ReactComponent as PencilIcon } from '@/assets/icon/pencil_icon.svg';
+import { ReactComponent as DogIcon } from '@/assets/icon/dog_icon.svg';
+
 import DailyCalendar from './components/DailyCalendar';
 import DailyAddWalkModal from './components/DailyAddWalk';
 import DailyAddFeed from './components/DailyAddFeed';
@@ -32,15 +32,25 @@ export default function DailyMain() {
     useLazyGetDailyListQuery();
   const [getDaily, { data: daily, isSuccess: dailySuccess }] = useLazyGetDailyQuery();
 
+  const [activeStartDate, setActiveStartDate] = useState<Date | undefined>(currentDate);
   const [todayDaily, setTodayDaily] = useState<DailyItemType | undefined>();
-  const [calendarOpen, setCalendarOpen] = useState(false);
   const [modalOpen, setModalOpen] = useState<'' | 'note' | 'walk' | 'feed' | 'bodyWeight'>('');
+  const [openSelectMenu] = useSelectModal();
 
-  const openDateSelector = () => setCalendarOpen(true);
-  const closeDateSelector = () => setCalendarOpen(false);
   const openModal = (content: 'note' | 'walk' | 'feed' | 'bodyWeight') => setModalOpen(content);
   const closeModal = () => setModalOpen('');
 
+  const handleRecordWalk = async () => {
+    const selectedMenu = await openSelectMenu(['산책하기', '간편기록']);
+    if (!selectedMenu) return;
+
+    if (selectedMenu === '산책하기') {
+      navigate('/daily/walk');
+    }
+    if (selectedMenu === '간편기록') {
+      openModal('walk');
+    }
+  };
   useEffect(() => {
     if (!queryStringDate) {
       const today = new Date();
@@ -61,6 +71,14 @@ export default function DailyMain() {
       setCurrentDate(date);
     }
   }, [searchParams]);
+  useEffect(() => {
+    if (!currentPetId) return;
+    if (listDate !== activeStartDate) {
+      console.log('fetch!!');
+      setListDate(activeStartDate);
+      getDailyList({ petId: currentPetId, date: dayjs(activeStartDate).format('YYYY-MM') });
+    }
+  }, [activeStartDate]);
 
   useEffect(() => {
     if (!currentDate || !dailyList || !currentPetId || !dailyListSuccess) return;
@@ -82,110 +100,40 @@ export default function DailyMain() {
 
     setTodayDaily(daily);
   }, [dailySuccess, daily]);
+
   if (!currentPetId) return null;
   return (
     <Layout header footer title="데일리 기록">
-      <div className="py-2 px-4 bg-white flex flex-col items-center justify-center gap-6 w-full h-full">
-        <div className="flex flex-col border rounded-[10px] h-3/4 max-h-[600px] w-full bg-white shadow-lg rounded-[10px]">
-          <div className="w-full h-14 bg-primary-dark shadow-md rounded-t-[10px] flex items-center justify-center">
-            <button onClick={openDateSelector}>
-              <h4 className="text-white font-[400] text-lg">
-                {dayjs(currentDate).format('YYYY. MM. DD')}
-              </h4>
-            </button>
-          </div>
-          <div className="bg-white flex-1 flex flex-col gap-4 p-2 px-10 relative rounded-b-[10px] overflow-y-scroll">
-            <div className="flex pt-8 pb-4 flex-col items-start w-full gap-2">
-              <h5 className="text-left text-md w-full text-primary-dark">오늘의 일기</h5>
-              {todayDaily?.data.note && (
-                <p className="text-gray-500 text-sm">{todayDaily.data.note}</p>
-              )}
-              {!todayDaily?.data.note && (
-                <p className="text-gray-500 text-sm">작성 된 일기가 없어요. 일기를 작성해보세요!</p>
-              )}
-            </div>
-            <div className="py-4 flex flex-col gap-2">
-              <h5 className="text-primary-dark">몸무게</h5>
-              <div className="w-full rounded-[10px] ">
-                {!todayDaily?.data.bodyWeight && (
-                  <p className="text-gray-500 text-sm">오늘 몸무게를 측정하지 않으셨어요!</p>
-                )}
-                {todayDaily?.data.bodyWeight && (
-                  <p className="text-gray-500 text-sm">{todayDaily?.data.bodyWeight}kg</p>
-                )}
-              </div>
-            </div>
-            <div className="py-4 flex flex-col gap-2">
-              <h5 className="text-primary-dark">급여량</h5>
-              <div className="flex gap-1 items-center justify-between">
-                {todayDaily?.data.feed && (
-                  <>
-                    <div className="w-3/4 h-3 bg-primary-main rounded-[10px]"></div>
-                    <span className="text-sm">{todayDaily.data.feed}g</span>
-                  </>
-                )}
-                {!todayDaily?.data.feed && (
-                  <p className="text-gray-500 text-sm">오늘 급여량을 입력하지 않으셨어요!</p>
-                )}
-              </div>
-            </div>
-            <div className="py-4 flex flex-col gap-2">
-              <h5 className="text-primary-dark">산책</h5>
-              {todayDaily?.data.walkDistance && (
-                <div className="flex flex-col gap-2">
-                  <div className="w-full flex gap-2">
-                    <div>
-                      <CalendarPrimary />
-                    </div>
-                    <div className="flex gap-1">
-                      <button>
-                        <span>산책 01</span>
-                      </button>
-                    </div>
-                  </div>
-                  <div className="flex items-center justify-center w-full gap-2">
-                    <span>{todayDaily.data.walkDistance}km</span>
-                    <span>{todayDaily.data.walkTime}min</span>
-                  </div>
-                </div>
-              )}
-              {!todayDaily?.data.walkDistance && (
-                <p className="text-gray-500 text-sm">오늘 산책을 하지 않으셨어요!</p>
-              )}
-            </div>
-            {calendarOpen && currentDate && (
-              <DailyCalendar
-                currentDate={currentDate}
-                closeCalendar={closeDateSelector}
-                dailyList={dailyList?.idAndDates ?? []}
-              />
-            )}
-          </div>
+      <div className="p-4 bg-white flex flex-col items-center gap-4 w-full h-full overflow-y-auto">
+        <div className="flex flex-col border w-full bg-white shadow-sm rounded-[10px]">
+          {currentDate && (
+            <DailyCalendar
+              currentDate={currentDate}
+              dailyList={dailyList?.idAndDates ?? []}
+              setActiveStartDate={setActiveStartDate}
+            />
+          )}
         </div>
-        <div className="flex gap-4 items-center w-full justify-center ">
+        <div className="flex w-full items-center gap-5">
           <button
-            className="rounded-[10px] shadow-md w-12 h-12 flex items-center justify-center bg-primary-dark"
-            onClick={() => openModal('note')}
+            type="button"
+            className="flex-1 rounded-[10px] bg-gray-200 h-20 p-4 shadow-md flex gap-4 items-center"
+            onClick={handleRecordWalk}
           >
-            <PencilIcon />
+            <div className="bg-primary-dark w-12 h-12 flex items-center justify-center rounded-[10px]">
+              <DogIcon />
+            </div>
+            <span className="text-black text-xl">산책기록</span>
           </button>
           <button
-            className="rounded-[10px] shadow-md w-12 h-12 flex items-center justify-center bg-primary-dark"
-            onClick={() => openModal('bodyWeight')}
+            type="button"
+            className="flex-1 rounded-[10px] bg-gray-200 h-20 p-4 shadow-md flex gap-4 items-center"
+            onClick={() => setModalOpen('note')}
           >
-            <WeightIcon style={{ fill: '#ffffff', width: 31, height: 31 }} />
-          </button>
-          <button
-            className="rounded-[10px] shadow-md w-12 h-12 flex items-center justify-center bg-primary-dark"
-            onClick={() => openModal('feed')}
-          >
-            <FoodIcon style={{ width: 31, height: 31, fill: '#ffffff' }} />
-          </button>
-          <button
-            className="rounded-[10px] shadow-md w-12 h-12 flex items-center justify-center bg-primary-dark"
-            onClick={() => openModal('walk')}
-          >
-            <DogIcon />
+            <div className="bg-primary-dark w-12 h-12 flex items-center justify-center rounded-[10px]">
+              <PencilIcon />
+            </div>
+            <span className="text-black text-xl">건강기록</span>
           </button>
         </div>
       </div>
--- src/pages/Daily/components/DailyCalendar.tsx
+++ src/pages/Daily/components/DailyCalendar.tsx
@@ -1,23 +1,25 @@
+import { Dispatch, SetStateAction } from 'react';
+import { useNavigate } from 'react-router-dom';
 import dayjs from 'dayjs';
 import Calendar from 'react-calendar';
+
 import { useAppDispatch } from '@/store/config';
-import { getDateString } from '@/utils/libs/date';
-import { useNavigate } from 'react-router-dom';
+import { DailyListItemType } from '@/store/api/dailyApi';
 import { setDate } from '@/store/slices/dailySlice';
+import { getDateString } from '@/utils/libs/date';
 
 import 'react-calendar/dist/Calendar.css';
 import '../calendar.css';
-import { DailyListItemType } from '@/store/api/dailyApi';
 
 type DailyCalendarProps = {
   currentDate: Date;
   dailyList: DailyListItemType[];
-  closeCalendar: () => void;
+  setActiveStartDate: Dispatch<SetStateAction<Date | undefined>>;
 };
 export default function DailyCalendar({
   currentDate,
-  closeCalendar,
   dailyList,
+  setActiveStartDate,
 }: DailyCalendarProps) {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
@@ -25,42 +27,35 @@ export default function DailyCalendar({
     const dateString = getDateString(selectedDate);
     dispatch(setDate({ date: dateString }));
     navigate(`/daily?date=${dateString}`, { replace: true });
-    closeCalendar();
   };
 
   return (
-    <div className="z-10 bg-white absolute top-0 w-full left-0 bottom-0">
-      <div
-        className={
-          'overflow-hidden transition-all flex items-start justify-center rounded-[10px] w-full'
+    <Calendar
+      calendarType="US"
+      value={currentDate}
+      defaultValue={currentDate}
+      onChange={changeDate}
+      onActiveStartDateChange={({ activeStartDate }) => setActiveStartDate(activeStartDate)}
+      formatDay={(locale, date) => dayjs(date).format('DD')}
+      minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+      maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+      tileContent={({ date }) => {
+        const dateString = getDateString(date);
+        const isExist = dailyList.find((daily) => daily.date === dateString);
+        // 날짜 타일에 컨텐츠 추가하기 (html 태그)
+        // 추가할 html 태그를 변수 초기화
+        const html = [];
+        if (isExist) {
+          html.push(
+            <div
+              key={dateString}
+              className="z-0 opacity-50 rounded-full absolute w-10 h-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2  bg-primary-light"
+            ></div>,
+          );
         }
-      >
-        <Calendar
-          value={currentDate}
-          defaultValue={currentDate}
-          onChange={changeDate}
-          formatDay={(locale, date) => dayjs(date).format('DD')}
-          minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
-          maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
-          tileContent={({ date }) => {
-            const dateString = getDateString(date);
-            const isExist = dailyList.find((daily) => daily.date === dateString);
-            // 날짜 타일에 컨텐츠 추가하기 (html 태그)
-            // 추가할 html 태그를 변수 초기화
-            const html = [];
-            if (isExist) {
-              html.push(
-                <div
-                  key={dateString}
-                  className="z-0 opacity-50 rounded-full absolute w-10 h-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2  bg-primary-light"
-                ></div>,
-              );
-            }
-            // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
-            return <div className="h-full flex flex-col justify-around items-center">{html}</div>;
-          }}
-        />
-      </div>
-    </div>
+        // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
+        return <div className="h-full flex flex-col justify-around items-center">{html}</div>;
+      }}
+    />
   );
 }
