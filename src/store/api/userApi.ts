76707ca231ae70d8e19fa930cb50b3f8ccf0f5fe
은승균 <dmstmdrbs98@gmail.com>
2022-09-07 14:49:50 +0900

    hotfix: modify api routes

--- src/store/api/userApi.ts
+++ src/store/api/userApi.ts
@@ -7,13 +7,13 @@ import { IGenericResponse } from './types';
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUser: builder.query<IUser, void>({
-      query: () => '/users',
+      query: () => '/v1/users',
       transformResponse: (response: IGenericResponse<IUser>) => response.data,
       providesTags: [{ type: 'User' }],
     }),
     login: builder.mutation<IAuthenticatedUser, ILoginForm>({
       query: (credentials) => ({
-        url: '/users',
+        url: '/v1/users',
         method: 'POST',
         body: credentials,
       }),
@@ -21,14 +21,14 @@ export const userApiSlice = apiSlice.injectEndpoints({
     }),
     logout: builder.mutation<IGenericResponse<void>, void>({
       query: () => ({
-        url: '/users',
+        url: '/v1/users',
         method: 'DELETE',
       }),
       invalidatesTags: ['User'],
     }),
     signUp: builder.mutation<IGenericResponse<{ userId: number }>, ISignUpForm>({
       query: (data) => ({
-        url: '/users/new',
+        url: '/v1/users/new',
         method: 'POST',
         body: {
           username: data.username,
@@ -39,14 +39,14 @@ export const userApiSlice = apiSlice.injectEndpoints({
     }),
     withdrawal: builder.mutation<IGenericResponse<void>, void>({
       query: () => ({
-        url: '/users/withdrawal',
+        url: '/v1/users/withdrawal',
         method: 'DELETE',
       }),
     }),
     changeRepresentativePet: builder.mutation<IGenericResponse<void>, number>({
       query: (petId) => {
         return {
-          url: '/users/representative-pet',
+          url: '/v1/users/representative-pet',
           method: 'PATCH',
           body: {
             representativePetId: petId,
