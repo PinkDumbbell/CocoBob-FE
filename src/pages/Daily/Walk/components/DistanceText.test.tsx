c4473a489365a01f5f92dfe3209f393bb3543b84
은승균 <dmstmdrbs98@gmail.com>
2022-09-21 11:37:45 +0900

    CB-407 test: modify DistanceText unit test
    DistanceText 유닛테스트 수정
    - 리팩토링

--- src/pages/Daily/Walk/components/DistanceText.test.tsx
+++ src/pages/Daily/Walk/components/DistanceText.test.tsx
@@ -8,18 +8,18 @@ describe('<DistanceText/>', () => {
     const distanceValue = 0.92; // 0.92km
     const distanceValueInMeter = getMeterDistance(distanceValue);
 
-    const button = render(<DistanceText distance={distanceValue} />);
+    const textElement = render(<DistanceText distance={distanceValue} />);
 
-    const renderedText = button.getByText('M').previousSibling?.textContent;
+    const renderedText = textElement.getByText('M').previousSibling?.textContent;
     expect(renderedText).toBe(distanceValueInMeter.toString());
   });
   test('KM 값 테스트', () => {
-    const distanceValue = 1.2345; // 1.2345km
+    const distanceValue = 1.23456; // 1.23456km
     const distanceFixedTwo = distanceValue.toFixed(2); // 1.23km
 
-    const button = render(<DistanceText distance={distanceValue} />);
+    const textElement = render(<DistanceText distance={distanceValue} />);
 
-    const renderedText = button.getByText('km').previousSibling?.textContent;
+    const renderedText = textElement.getByText('km').previousSibling?.textContent;
     expect(renderedText).toBe(distanceFixedTwo);
   });
 });
