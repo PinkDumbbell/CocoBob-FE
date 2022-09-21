7400302ede4897d98cb329a38ef88fce0089a2a0
은승균 <dmstmdrbs98@gmail.com>
2022-09-21 15:51:51 +0900

    CB-401 refactor: modify WalkHistory page
    산책 기록 페이지 수정
    - 컴포넌트 분리
    - 로직 코드 수정
      - useMemo로 total 정보 저장
    	- literal Object를 Map으로 수정

--- src/pages/Daily/Walk/WalkHistoryPage.tsx
+++ src/pages/Daily/Walk/WalkHistoryPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import Button from '@/components/Button';
@@ -6,6 +6,7 @@ import Layout from '@/components/layout/Layout';
 import { useAppSelector } from '@/store/config';
 import { getCurrentPet } from '@/store/slices/userSlice';
 import DailyAddWalkModal from '../components/DailyAddWalk';
+import WalkHistoryList, { WalkHistoryItemType } from './components/WalkHistoryList';
 
 const AddWalkButton = ({ onClick }: { onClick: () => void }) => {
   return (
@@ -14,17 +15,104 @@ const AddWalkButton = ({ onClick }: { onClick: () => void }) => {
     </button>
   );
 };
-
+const TodayWalkInfo = ({
+  totalDistance,
+  totalTime,
+}: {
+  totalDistance: number;
+  totalTime: number;
+}) => {
+  return (
+    <>
+      <h4>오늘은 이만큼 걸었어요!</h4>
+      <div className="flex space-x-5 font-semibold">
+        <div className="flex  items-end">
+          <span className="text-2xl">{totalDistance}</span>
+          <span className="text-primary-dark">KM</span>
+        </div>
+        <div className="flex space-x-1">
+          <div className="flex items-end">
+            <span className="text-2xl">{Math.floor(totalTime / 60)}</span>
+            <span className="text-primary-dark">시간</span>
+          </div>
+          <div className="flex items-end">
+            <span className="text-2xl">{totalTime % 60}</span>
+            <span className="text-primary-dark">분</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+const mockWalkHistory: WalkHistoryItemType[] = [
+  {
+    id: 1,
+    date: new Date(),
+    walkDistance: 1.4,
+    endTime: '16:54',
+    startTime: '16:12',
+    walkTime: 42,
+  },
+  {
+    id: 2,
+    date: new Date(),
+    walkDistance: 2.8,
+    endTime: '12:54',
+    startTime: '14:02',
+    walkTime: 52,
+  },
+  {
+    id: 3,
+    date: new Date(),
+    walkDistance: 1.2,
+    endTime: '13:08',
+    startTime: '13:42',
+    walkTime: 34,
+  },
+  {
+    id: 4,
+    date: new Date(),
+    walkDistance: 1.2,
+    endTime: '13:08',
+    startTime: '13:42',
+    walkTime: 34,
+  },
+];
 export default function WalkHistoryPage() {
-  const walkHistory = [];
   const currentPetId = useAppSelector(getCurrentPet);
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const date = searchParams.get('date');
   const [addWalkModal, setAddWalkModal] = useState(false);
+  const [walkHistory, setWalkHistory] = useState(mockWalkHistory);
+  const isWalkHistoryExist = walkHistory.length > 0;
+
+  const initWalkTotalInfo = new Map<'totalTime' | 'totalDistance', number>([
+    ['totalTime', 0],
+    ['totalDistance', 0],
+  ]);
+  const walkTotalInfo = useMemo(
+    () =>
+      walkHistory.reduce((walkTotal, currentHistory) => {
+        const newTotalTime = (walkTotal.get('totalTime') ?? 0) + currentHistory.walkTime;
+        const newTotalDistance =
+          (walkTotal.get('totalDistance') ?? 0) + currentHistory.walkDistance;
+        walkTotal.set('totalTime', newTotalTime);
+        walkTotal.set('totalDistance', newTotalDistance);
+        return walkTotal;
+      }, initWalkTotalInfo),
+    walkHistory,
+  );
+
+  const totalDistance = walkTotalInfo.get('totalDistance') ?? 0;
+  const totalTime = walkTotalInfo.get('totalTime') ?? 0;
 
   const goDailyPage = () => navigate(`/daily?date=${date}`);
   const goWalkRecordPage = () => navigate(`/daily/walk/record?date=${date}`);
+
+  const addWalkRecord = (newWalkRecord: WalkHistoryItemType) => {
+    setWalkHistory((prevHistory) => [newWalkRecord].concat(prevHistory));
+  };
 
   if (!currentPetId)
     return (
@@ -44,15 +132,22 @@ export default function WalkHistoryPage() {
         </div>
       }
     >
-      <div className="w-full max-w-[425px] h-full max-auto flex flex-col p-4">
+      <div className="w-full max-w-[425px] h-full max-auto flex flex-col p-4 space-y-4">
         <div className="w-full p-4 rounded-[10px] bg-gray-300 flex flex-col items-center space-y-4">
-          {walkHistory.length === 0 && <h4>아직 산책기록이 없어요</h4>}
+          {!isWalkHistoryExist && <h4>아직 산책기록이 없어요</h4>}
+          {isWalkHistoryExist && (
+            <TodayWalkInfo totalDistance={totalDistance} totalTime={totalTime} />
+          )}
           <Button label="산책하기" onClick={goWalkRecordPage} />
+        </div>
+        <div className="w-full overflow-y-auto">
+          <WalkHistoryList walkHistory={walkHistory} />
         </div>
       </div>
       {addWalkModal && (
         <DailyAddWalkModal
           closeModal={() => setAddWalkModal(false)}
+          onSave={addWalkRecord}
           date={new Date(date ?? new Date())}
           petId={currentPetId}
         />
