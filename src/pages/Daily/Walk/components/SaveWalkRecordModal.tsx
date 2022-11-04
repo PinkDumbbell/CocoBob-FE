d67a8ec527061ddeb1de64cae811173b55aef76a
은승균 <dmstmdrbs98@gmail.com>
2022-11-04 14:20:09 +0900

    CB-505 fix: save walk params format error
    저장 시간 단위 변경
    - 시간 단위가 초단위까지 필요함.

--- src/pages/Daily/Walk/components/SaveWalkRecordModal.tsx
+++ src/pages/Daily/Walk/components/SaveWalkRecordModal.tsx
@@ -1,3 +1,5 @@
+/* global kakao */
+import { useRef, useEffect } from 'react';
 import { WalkRecordType } from '@/@type/walk';
 import Button from '@/components/Button';
 import Header from '@/components/layout/Header';
@@ -10,13 +12,37 @@ type SaveWalkModalProps = {
   walkData: WalkRecordType;
 };
 export default function SaveWalkModal({ close, save, walkData }: SaveWalkModalProps) {
+  const staticMap = useRef(null);
+
+  useEffect(() => {
+    if (!staticMap.current) {
+      return;
+    }
+    // eslint-disable-next-line
+    new kakao.maps.StaticMap(staticMap.current, {
+      center: new kakao.maps.LatLng(33.45071, 126.570667),
+      level: 3,
+      marker: false,
+    });
+  }, [staticMap.current]);
+
   return (
-    <div className="fixed top-0 w-full max-w-[425px] h-full bg-white z-[9999] pt-[50px] pb-[80px]">
+    <div className="fixed top-0 w-full max-w-[425px] h-full bg-white z-[9999] pt-[50px] flex flex-col">
       <Header canGoBack onClickGoBack={close} title="산책저장" />
-      <div className=" bg-white w-full h-full max-w-[425px] p-4">
+      <div className="p-4 flex-1">
+        <div className="rounded-[10px] overflow-hidden w-full h-auto">
+          <div
+            ref={staticMap}
+            onClickCapture={(e) => {
+              e.stopPropagation();
+            }}
+            className="w-full h-full"
+          ></div>
+        </div>
+
         <WalkRecordDetail walkRecord={walkData} />
       </div>
-      <div className="w-full flex p-4 items-center justify-around">
+      <div className="w-full p-4">
         <Button width="full" label="저장하기" onClick={save} />
       </div>
     </div>
