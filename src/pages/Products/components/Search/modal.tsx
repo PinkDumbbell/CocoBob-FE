799ee6a393ae0a141f711f6d8601685962491106
taehong0-0 <xoghd5885@ajou.ac.kr>
2022-11-08 22:44:08 +0900

    CB-502 feat: modify search keyword - 키워드 검색 로직 수정 - search modal 헤더 분리하여 검색 부분 수정

--- src/pages/Products/components/Search/modal.tsx
+++ src/pages/Products/components/Search/modal.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import Portal from '@/portal';
-import SearchHeader from '@/pages/Products/components/Search/header';
 import RecommandContent from '@/pages/Products/components/Search';
 
-export default function SearchModal({
-  onClose,
-  searchInputValue,
-  setSearchInputValue,
-  onClickSearch,
-}: any) {
+export default function SearchModal({ searchKeyword, onClickSearch }: any) {
   return (
     <Portal>
-      <div className="fixed top-0 left-0 right-0 mx-auto flex flex-col w-full max-w-[425px] bg-white">
-        <SearchHeader
-          searchInputValue={searchInputValue}
-          setSearchInputValue={setSearchInputValue}
-          onClickSearch={onClickSearch}
-          goBack={onClose}
-        />
+      <div className="fixed top-[50px] left-0 right-0 mx-auto flex flex-col w-full max-w-[425px] bg-white">
         {
           <div className="w-full h-screen">
-            <RecommandContent searchInputValue={searchInputValue} onClickSearch={onClickSearch} />
+            <RecommandContent searchInputValue={searchKeyword} onClickSearch={onClickSearch} />
           </div>
         }
       </div>
