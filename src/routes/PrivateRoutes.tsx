315ff0691a53367030c301c515f808602eb77256
은승균 <dmstmdrbs98@gmail.com>
2022-11-12 01:16:37 +0900

    CB-543 feat: page lazy load
    페이지 Lazy Loading 적용
    - chunk 파일 분리

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,30 +1,33 @@
+import { lazy, Suspense } from 'react';
 import { Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom';
 
+import { Spinner } from '@/Animation';
 import useUser from '@/utils/hooks/useUser';
-import MainPage from '@/pages/Main';
-import RegisterPet from '@/pages/RegisterPet';
-import ProductsPage from '@/pages/Products';
 
-import Mypage from '@/pages/Mypage';
-import MypageMain from '@/pages/Mypage/Main';
-import ProfilePage from '@/pages/Mypage/Profile';
-import WishPage from '@/pages/Mypage/Wish';
-import PetsPage from '@/pages/Mypage/pets/index';
-import PetInfo from '@/pages/Mypage/pets/[id]';
-import PetInfoEdit from '@/pages/Mypage/pets/[id]/edit';
-import NotFound from '@/pages/404';
-import ProductDetailPage from '@/pages/Products/ProductDetail';
 import Daily from '@/pages/Daily';
 import DailyMain from '@/pages/Daily/DailyPage';
-import DailyWalk from '@/pages/Daily/Walk';
-import WalkRecordPage from '@/pages/Daily/Walk/WalkRecordPage';
+import MainPage from '@/pages/Main';
+import Mypage from '@/pages/Mypage';
+import RegisterPet from '@/pages/RegisterPet';
 import RecommendProducts from '@/pages/Products/Recoomend';
-import NoteAddPage from '@/pages/Daily/Note/NoteWritePage';
-import NotePage from '@/pages/Daily/Note/NotePage';
-import WalkHistoryPage from '@/pages/Daily/Walk/WalkHistoryPage';
-import WalkHisotyDetailPage from '@/pages/Daily/Walk/[id]';
-import HealthRecords from '@/pages/Daily/HealthRecords';
-import HealthRecordsPage from '@/pages/Daily/HealthRecords/HealthRecordsPage';
+
+const DailyWalk = lazy(() => import('@/pages/Daily/Walk'));
+const HealthRecords = lazy(() => import('@/pages/Daily/HealthRecords'));
+const MypageMain = lazy(() => import('@/pages/Mypage/Main'));
+const ProfilePage = lazy(() => import('@/pages/Mypage/Profile'));
+const WishPage = lazy(() => import('@/pages/Mypage/Wish'));
+const PetsPage = lazy(() => import('@/pages/Mypage/pets'));
+const PetInfo = lazy(() => import('@/pages/Mypage/pets/[id]'));
+const PetInfoEdit = lazy(() => import('@/pages/Mypage/pets/[id]/edit'));
+const ProductsPage = lazy(() => import('@/pages/Products'));
+const ProductDetailPage = lazy(() => import('@/pages/Products/ProductDetail'));
+const NoteAddPage = lazy(() => import('@/pages/Daily/Note/NoteWritePage'));
+const NotePage = lazy(() => import('@/pages/Daily/Note/NotePage'));
+const WalkHistoryPage = lazy(() => import('@/pages/Daily/Walk/WalkHistoryPage'));
+const WalkHisotyDetailPage = lazy(() => import('@/pages/Daily/Walk/[id]'));
+const WalkRecordPage = lazy(() => import('@/pages/Daily/Walk/WalkRecordPage'));
+const HealthRecordsPage = lazy(() => import('@/pages/Daily/HealthRecords/HealthRecordsPage'));
+const NotFound = lazy(() => import('@/pages/404'));
 
 function PrivateRoute() {
   const { isLoggedIn } = useUser();
@@ -35,38 +38,40 @@ function PrivateRoute() {
 function PrivateRoutes() {
   const location = useLocation();
   return (
-    <Routes location={location} key={location.pathname}>
-      <Route element={<PrivateRoute />}>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/register" element={<RegisterPet />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<ProductDetailPage />} />
-        <Route path="/products/recommend" element={<RecommendProducts />} />
-        <Route path="/mypage" element={<Mypage />}>
-          <Route index element={<MypageMain />} />
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="wish" element={<WishPage />} />
-          <Route path="pets" element={<PetsPage />} />
-          <Route path="pets/:id" element={<PetInfo />} />
-          <Route path="pets/:id/edit" element={<PetInfoEdit />} />
-        </Route>
-        <Route path="/daily" element={<Daily />}>
-          <Route index element={<DailyMain />} />
-          <Route path="walk" element={<DailyWalk />}>
-            <Route index element={<WalkHistoryPage />} />
-            <Route path="record" element={<WalkRecordPage />} />
-            <Route path="record/:id" element={<WalkHisotyDetailPage />} />
+    <Suspense fallback={<Spinner />}>
+      <Routes location={location} key={location.pathname}>
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/register" element={<RegisterPet />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
+          <Route path="/products/recommend" element={<RecommendProducts />} />
+          <Route path="/mypage" element={<Mypage />}>
+            <Route index element={<MypageMain />} />
+            <Route path="profile" element={<ProfilePage />} />
+            <Route path="wish" element={<WishPage />} />
+            <Route path="pets" element={<PetsPage />} />
+            <Route path="pets/:id" element={<PetInfo />} />
+            <Route path="pets/:id/edit" element={<PetInfoEdit />} />
           </Route>
-          <Route path="health" element={<HealthRecords />}>
-            <Route index element={<HealthRecordsPage />} />
+          <Route path="/daily" element={<Daily />}>
+            <Route index element={<DailyMain />} />
+            <Route path="walk" element={<DailyWalk />}>
+              <Route index element={<WalkHistoryPage />} />
+              <Route path="record" element={<WalkRecordPage />} />
+              <Route path="record/:id" element={<WalkHisotyDetailPage />} />
+            </Route>
+            <Route path="health" element={<HealthRecords />}>
+              <Route index element={<HealthRecordsPage />} />
+            </Route>
+            <Route path="note/:id" element={<NotePage />} />
+            <Route path="note/edit" element={<NoteAddPage />} />
+            <Route path="note/new" element={<NoteAddPage />} />
           </Route>
-          <Route path="note/:id" element={<NotePage />} />
-          <Route path="note/edit" element={<NoteAddPage />} />
-          <Route path="note/new" element={<NoteAddPage />} />
         </Route>
-      </Route>
-      <Route element={<NotFound />} path="*" />
-    </Routes>
+        <Route element={<NotFound />} path="*" />
+      </Routes>
+    </Suspense>
   );
 }
 export default PrivateRoutes;
