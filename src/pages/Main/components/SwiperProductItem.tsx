9645a0a97cd2e590632555f25364a809eba3875b
은승균 <dmstmdrbs98@gmail.com>
2022-11-19 18:40:37 +0900

    CB-550 style: add min height for blocking layout shift in swiper item
    레이아웃 변경을 방지하기 위한 min-height 속성 부여

--- src/pages/Main/components/SwiperProductItem.tsx
+++ src/pages/Main/components/SwiperProductItem.tsx
@@ -23,9 +23,11 @@ export default function SwiperProductItem({ product, isActive }: SwiperProductIt
     >
       <ContentsContainer>
         <VerticalBox className="flex-1 justify-between relative overflow-hidden text-ellipsis whitespace-nowrap">
-          <VerticalCenterBox onClick={goProductDetailPage}>
-            <img className="rounded-t" src={thumbnail} alt={name} />
-          </VerticalCenterBox>
+          <div className="min-h-[60%]">
+            <VerticalCenterBox onClick={goProductDetailPage}>
+              <img className="rounded-t" src={thumbnail} alt={name} />
+            </VerticalCenterBox>
+          </div>
           <div className="p-1 flex flex-col flex-1 justify-between">
             <VerticalBox className="overflow-hidden py-1 space-y-1" onClick={goProductDetailPage}>
               <p className="text-[11px] leading-[11px] text-primary-dark font-semibold">{brand}</p>
