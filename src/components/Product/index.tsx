2a950df8fca99229c9d4582b65948af0f8b9a9cb
은승균 <dmstmdrbs98@gmail.com>
2022-11-23 16:17:26 +0900

    CB-559 style: add highlight in AAFCO text

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -13,7 +13,7 @@ import { ProductPreviewType } from '@/@type/product';
 function getAge(growing?: boolean, aged?: boolean) {
   if (growing) return <p className="text-primary-bright">성장기</p>;
   if (aged) return <p className="text-[#D27C1A]">노령견</p>;
-  return <p className="text-gray">노령견</p>;
+  return <p className="text-gray">성견</p>;
 }
 interface IProductItem {
   product: ProductPreviewType;
@@ -28,9 +28,13 @@ function ProductItem(props: IProductItem) {
       <div className="flex flex-col p-2 gap-2 h-full justify-between">
         <div>
           {product.isAAFCOSatisfied ? (
-            <p className="text-primary-dark text-label font-medium">AAFCO 충족</p>
+            <p className="text-primary-dark text-label font-medium relative after:rounded after:block after:absolute after:left-0 after:opacity-25 after:top-2.5 after:bg-primary-brightest after:py-1.5 after:px-9">
+              AAFCO 충족
+            </p>
           ) : (
-            <p className="text-gray text-label font-medium">AAFCO 미달</p>
+            <p className="text-gray text-label font-medium relative after:rounded after:block after:absolute after:left-0 after:opacity-25 after:top-2.5 after:bg-secondary-brighter after:py-1.5 after:px-9">
+              AAFCO 미달
+            </p>
           )}
           <h4 className="text-black font-bold">{product?.name}</h4>
         </div>
