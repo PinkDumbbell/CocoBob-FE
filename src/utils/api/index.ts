548955aab7cd65ceaa596ccaa0c27df4e26e9ecb
Jae Han Song <rfv1479@gmail.com>
2022-12-21 16:38:20 +0900

    hotfix : extend axios timeout

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
-  timeout: 2000,
+  timeout: 20000,
 });
 
 export default instance;
